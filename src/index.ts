5b76efd12f867e120e39614d49494cbf408404d3
steins0668 <archelausagaton@gmail.com>
2025-10-07 22:37:48 +0800

    feat: add index file

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cors, { type CorsOptions } from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const whitelist = ["http://localhost:[port]"];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) callback(null, true);
+    else callback(new Error("Not allowed by CORS."));
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); // json parsing
+
+app.use(cookieParser());
+
+app.use("/auth");
+
+const port = 2620; //  can be anything
+app.listen(port, "0.0.0.0", () => {
+  //  the 0.0.0.0 is for hosting on your local ip
+  console.log("Server is now running at port: " + port);
+});
