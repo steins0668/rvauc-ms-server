6823b91e83e05555d46335dbb9040529b1ac241d
steins0668 <archelausagaton@gmail.com>
2025-10-14 22:12:13 +0800

    feat: add `SessionManager` service

---
+++ src/features/auth/services/session-manager/session-starter.ts
@@ -0,0 +1,152 @@
+import { randomUUID } from "crypto";
+import { TxContext } from "../../../../db/create-context";
+import { DbAccess } from "../../../../error";
+import { HashUtil, ResultBuilder } from "../../../../utils";
+import { Session } from "../../error";
+import { SessionResult } from "../../types";
+import { SessionTokenRepository, UserSessionRepository } from "../repositories";
+
+/**
+ * @class
+ * @description Handles user session management from starting sessions, modifying
+ * existing sessions, as well as ending sessions.
+ */
+export class SessionStarter {
+  private readonly _sessionTokenRepository: SessionTokenRepository;
+  private readonly _userSessionRepository: UserSessionRepository;
+
+  public constructor(
+    sessionTokenRepository: SessionTokenRepository,
+    userSessionRepository: UserSessionRepository
+  ) {
+    this._sessionTokenRepository = sessionTokenRepository;
+    this._userSessionRepository = userSessionRepository;
+  }
+
+  //  todo: add docs
+  public async newSession(sessionData: {
+    userId: number;
+    sessionNumber: string;
+    refreshToken: string;
+    expiresAt?: Date | null;
+  }): Promise<
+    SessionResult.Success<string, "SESSION_START"> | SessionResult.Fail
+  > {
+    const {
+      userId,
+      sessionNumber,
+      refreshToken,
+      expiresAt = null,
+    } = sessionData;
+
+    try {
+      const result = await this._userSessionRepository.execTransaction(
+        async (tx) => {
+          const sessionId = await this.createSession({
+            tx,
+            sessionNumber,
+            userId,
+            expiresAt,
+          });
+
+          //  store first refresh token
+          await this.storeRefreshTkn({ tx, sessionId, refreshToken });
+
+          return sessionNumber;
+        }
+      );
+
+      return ResultBuilder.success(result, "SESSION_START");
+    } catch (err) {
+      const error = Session.normalizeError({
+        name: "SESSION_START_ERROR",
+        message:
+          "An error occured while creating session. Please try again later.",
+        err,
+      });
+
+      return ResultBuilder.fail(error);
+    }
+  }
+
+  //#region newSession helpers
+  /**
+   * @description Create a `UserSession` object and insert it to the `user_sessions`
+   * table.
+   * @param options
+   * @returns
+   */
+  private async createSession(options: {
+    tx: TxContext;
+    sessionNumber: string;
+    userId: number;
+    expiresAt?: Date | null;
+  }): Promise<number> {
+    const { tx, userId, sessionNumber, expiresAt } = options;
+
+    const now = new Date();
+    const nowISO = now.toISOString();
+    //  create session
+    const sessionId = await this._userSessionRepository.insertSession({
+      dbOrTx: tx,
+      userSession: {
+        userId,
+        sessionHash: HashUtil.byCrypto(sessionNumber),
+        createdAt: nowISO,
+        lastUsedAt: nowISO,
+        expiresAt: expiresAt?.toISOString() ?? null,
+      },
+    });
+
+    if (sessionId === undefined)
+      throw new DbAccess.ErrorClass({
+        name: "DB_ACCESS_INSERT_ERROR",
+        message: "Failed storing session to database.",
+      });
+
+    return sessionId;
+  }
+
+  /**
+   * @public
+   * @function generateSessionNumber
+   * @description Generates a session number for the provided `userId`.
+   * Uses the `userId`, current date and a random generated `UUID`.
+   * @param userId The `id` of the user.
+   * @returns A string representing the generated session number.
+   */
+  public generateSessionNumber(userId: number): string {
+    const sessionNumber = `${userId}-${Date.now()}-${randomUUID()}`;
+    return sessionNumber;
+  }
+
+  /**
+   * @description Create a `SessionToken` object and insert it to the
+   * `session_tokens` table.
+   * @param tx
+   * @param tknData
+   */
+  private async storeRefreshTkn(tknData: {
+    tx: TxContext;
+    sessionId: number;
+    refreshToken: string;
+  }): Promise<void> {
+    const { tx, sessionId, refreshToken } = tknData;
+
+    const tknId = await this._sessionTokenRepository.insertToken({
+      dbOrTx: tx,
+      sessionToken: {
+        sessionId,
+        tokenHash: HashUtil.byCrypto(refreshToken),
+        createdAt: new Date().toISOString(),
+      },
+    });
+
+    if (tknId === undefined)
+      throw new DbAccess.ErrorClass({
+        name: "DB_ACCESS_INSERT_ERROR",
+        message: "Failed storing refresh token to database.",
+      });
+  }
+  //#endregion
+}
---
+++ src/features/auth/services/session-manager/session-token-rotator.ts
@@ -0,0 +1,192 @@
+import { TxContext } from "../../../../db/create-context";
+import { DbAccess } from "../../../../error";
+import { HashUtil, ResultBuilder } from "../../../../utils";
+import { Session } from "../../error";
+import { SessionResult, ViewModels } from "../../types";
+import { SessionTokenRepository, UserSessionRepository } from "../repositories";
+
+export class SessionTokenRotator {
+  private readonly _sessionTokenRepository: SessionTokenRepository;
+  private readonly _userSessionRepository: UserSessionRepository;
+
+  constructor(
+    sessionTokenRepository: SessionTokenRepository,
+    userSessionTokenRepository: UserSessionRepository
+  ) {
+    this._sessionTokenRepository = sessionTokenRepository;
+    this._userSessionRepository = userSessionTokenRepository;
+  }
+
+  public async rotate(data: {
+    sessionNumber: string;
+    oldToken: string;
+    newToken: string;
+  }): Promise<
+    SessionResult.Success<number, "SESSION_TOKEN_ROTATION"> | SessionResult.Fail
+  > {
+    const { sessionNumber, oldToken, newToken } = data;
+
+    try {
+      const result = await this._userSessionRepository.execTransaction(
+        async (tx) => {
+          //  updated last used for session
+          const updatedId = await this.updateLastUsed({ tx, sessionNumber });
+
+          //  invalidate old token
+          await this.invalidateToken({ tx, oldToken });
+
+          const newTknHash = HashUtil.byCrypto(newToken);
+
+          await this.ensureTokenUnused({ tx, tknHash: newTknHash });
+
+          //  add new token
+          await this.storeNewTkn({
+            tx,
+            sessionId: updatedId,
+            tokenHash: newTknHash,
+          });
+
+          return updatedId;
+        }
+      );
+
+      return ResultBuilder.success(result, "SESSION_TOKEN_ROTATION");
+    } catch (err) {
+      const error = Session.normalizeError({
+        name: "SESSION_TOKEN_ROTATION_ERROR",
+        message: "Failed rotating tokens. Please try again later.",
+        err,
+      });
+
+      return ResultBuilder.fail(error);
+    }
+  }
+  //#region updateSession helpers
+  /**
+   * Update `lastUsed` field for session with the matching hash of the provided
+   * `sessionNumber`.
+   * @param options
+   * @returns
+   */
+  private async updateLastUsed(options: {
+    tx: TxContext;
+    sessionNumber: string;
+  }): Promise<number> {
+    const getUpdateErr = (cause?: unknown) => {
+      return new DbAccess.ErrorClass({
+        name: "DB_ACCESS_UPDATE_ERROR",
+        message: "Failed updating session.",
+        cause,
+      });
+    };
+
+    try {
+      //  updated last used for session
+      const updatedId = await this._userSessionRepository.updateLastUsed({
+        dbOrTx: options.tx,
+        queryBy: "session_hash",
+        sessionHash: HashUtil.byCrypto(options.sessionNumber),
+      });
+
+      if (updatedId === undefined) throw getUpdateErr();
+
+      return updatedId;
+    } catch (err) {
+      if (err instanceof DbAccess.ErrorClass) throw err;
+
+      throw getUpdateErr(err);
+    }
+  }
+
+  /**
+   * Invalidate the provided token string if found in the database by
+   * setting the `isUsed` field to `true`.
+   * @param options
+   */
+  private async invalidateToken(options: {
+    tx: TxContext;
+    oldToken: string;
+  }): Promise<void> {
+    try {
+      await this._sessionTokenRepository.invalidateTokens({
+        dbOrTx: options.tx,
+        queryBy: "token_hash",
+        tokenHash: HashUtil.byCrypto(options.oldToken),
+      });
+    } catch (err) {
+      throw new DbAccess.ErrorClass({
+        name: "DB_ACCESS_UPDATE_ERROR",
+        message: "Faield invalidating previous refresh token.",
+        cause: err,
+      });
+    }
+  }
+
+  /**
+   * Get the stored tokens in the database matching the provided hash.
+   * If one of them has the `isUsed` field set to `true`, throw an error.
+   * @param options
+   */
+  private async ensureTokenUnused(options: {
+    tx: TxContext;
+    tknHash: string;
+  }): Promise<void> {
+    let usedTokens: ViewModels.SessionToken[] = [];
+    try {
+      usedTokens = await this._sessionTokenRepository.getTokens({
+        dbOrTx: options.tx,
+        queryBy: "token_hash",
+        tokenHash: options.tknHash,
+      });
+    } catch (err) {
+      throw new DbAccess.ErrorClass({
+        name: "DB_ACCESS_QUERY_ERROR",
+        message: "Failed checking refresh tokens.",
+        cause: err,
+      });
+    }
+
+    //  todo: add fallback behavior to this (delete/logout all sessions)
+    if (usedTokens.some((token) => token.isUsed))
+      throw new Session.ErrorClass({
+        name: "SESSION_TOKEN_REUSE_ERROR",
+        message: "Token is already used.",
+      });
+  }
+
+  /**
+   * @description Create a new `SessionToken` object and insert it to the
+   * `session_tokens` table.
+   * @param options
+   */
+  private async storeNewTkn(options: {
+    tx: TxContext;
+    sessionId: number;
+    tokenHash: string;
+  }) {
+    const getInsertErr = (cause?: unknown) => {
+      return new DbAccess.ErrorClass({
+        name: "DB_ACCESS_INSERT_ERROR",
+        message: "Failed to store new token.",
+        cause,
+      });
+    };
+
+    try {
+      const newTknId = await this._sessionTokenRepository.insertToken({
+        dbOrTx: options.tx,
+        sessionToken: {
+          ...options,
+          createdAt: new Date().toISOString(),
+        },
+      });
+
+      if (newTknId === undefined) throw getInsertErr();
+    } catch (err) {
+      if (err instanceof DbAccess.ErrorClass) throw err;
+
+      throw getInsertErr(err);
+    }
+  }
+  //#endregion
+}
