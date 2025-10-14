ae014ad006dbf0713e74a952155d65a3d089b5c3
steins0668 <archelausagaton@gmail.com>
2025-10-14 21:58:48 +0800

    feat: add type for Authentication Tokens

--- src/features/auth/types/index.ts
+++ src/features/auth/types/index.ts
@@ -1,3 +1,4 @@
 export * as InsertModels from "./auth-insertmodels.type";
 export * as Tables from "./auth-tables.type";
+export * from "./auth-token.type";
 export * as ViewModels from "./auth-viewmodels.type";
