f7c7d0f9dc553cbef1280f9dad64e95afee93cfa
steins0668 <archelausagaton@gmail.com>
2025-10-31 01:13:47 +0800

    refactor: remove unused schemas and types for `RegistersSchemas` namespace

--- src/features/auth/schemas/register.schema.ts
+++ src/features/auth/schemas/register.schema.ts
@@ -3,51 +3,7 @@ import { ENUMS, REGEX } from "../data";
 
 const { USERNAME, PASSWORD, STUDENT_NUMBER } = REGEX.AUTH;
 
-export type RegisterSchema = z.infer<typeof registerSchema>;
-
-export const registerSchema = z.object({
-  roleId: z.number(),
-  email: z.email({
-    error: (iss) =>
-      iss.input === undefined ? "Email is required." : "Invalid email.",
-  }),
-  username: z
-    .string({
-      error: (iss) =>
-        iss.input === undefined ? "Username is required." : "Invalid username.",
-    })
-    .regex(USERNAME, { error: "Invalid username." }),
-
-  passwordHash: z
-    .string({
-      error: (iss) =>
-        iss.input === undefined ? "Password is required." : "Invalid Password",
-    })
-    .regex(PASSWORD, {
-      error: "Invalid password.",
-    }),
-  surname: z.string({
-    error: (iss) =>
-      iss.input === undefined ? "Surname is required." : "Invalid input.",
-  }),
-  firstName: z.string({
-    error: (iss) =>
-      iss.input === undefined ? "First name is required." : "Invalid input.",
-  }),
-  middleName: z.string().optional(),
-  //  todo: update register flow to include contact number and automatically insert new students/professors to their respective tables.
-  contactNumber: z.string({
-    error: (iss) =>
-      iss.input === undefined
-        ? "Contact number is required."
-        : "Invalid input.",
-  }),
-});
-
 export namespace RegisterSchemas {
-  export type User = z.infer<typeof user>;
-  export type Student = z.infer<typeof student>;
-  export type Professor = z.infer<typeof professor>;
   export type Base = z.infer<typeof base>;
 
   export const user = z.object({
@@ -92,57 +48,6 @@ export namespace RegisterSchemas {
           : "Invalid input.",
     }),
   });
-
-  export const student = z
-    .object({
-      ...user.shape,
-      //  todo: handle constraints for current valid ids
-      departmentId: z.number({
-        error: (iss) =>
-          iss.input === undefined
-            ? "Department is required."
-            : "Invalid department.",
-      }),
-
-      studentNumber: z
-        .string({
-          error: (iss) =>
-            iss.input === undefined
-              ? "Student number is required."
-              : "Invalid student number.",
-        })
-        .regex(STUDENT_NUMBER, { error: "Invalid student number." }),
-      yearLevel: z
-        .number()
-        .min(1, {
-          error: "Year must be between no lower than 1 and no higher than 4.",
-        })
-        .max(4, {
-          error: "Year must be between no lower than 1 and no higher than 4.",
-        }),
-
-      //  todo: add validation for available blocks
-      block: z.string(),
-    })
-    .strict();
-
-  export const professor = z
-    .object({
-      ...user.shape,
-      collegeId: z.number({
-        error: (iss) =>
-          iss.input === undefined
-            ? "College id is required."
-            : "Invalid college id.",
-      }),
-      facultyRank: z.string({
-        error: (iss) =>
-          iss.input === undefined
-            ? "Faculty rank is required."
-            : "Invalid faculty rank.",
-      }),
-    })
-    .strict();
 
   export const base = z.discriminatedUnion(
     "roleId",
@@ -200,11 +105,4 @@ export namespace RegisterSchemas {
         iss.input === undefined ? "Role id is required." : "Invalid role id.",
     }
   );
-
-  export const dictionary = { user, professor, student } as const;
-
-  export type Types =
-    | { type: "student"; schema: RegisterSchemas.Student }
-    | { type: "user"; schema: RegisterSchemas.User }
-    | { type: "professor"; schema: RegisterSchemas.Professor };
 }
