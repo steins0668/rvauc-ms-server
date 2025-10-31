98dbd53465a0a18ab0754fc64777ade9afacc08a
steins0668 <archelausagaton@gmail.com>
2025-10-31 22:25:31 +0800

    refactor: remove deprecated methods from `SessionToken` repository

--- src/features/auth/services/repositories/session-token.repository.ts
+++ src/features/auth/services/repositories/session-token.repository.ts
@@ -1,35 +1,14 @@
 import { and, eq, or, SQL } from "drizzle-orm";
-import { DbContext, TxContext } from "../../../../db/create-context";
+import { DbContext } from "../../../../db/create-context";
 import { sessionTokens } from "../../../../models";
 import { Repository } from "../../../../services";
 import {
-  InsertModels,
-  ViewModels,
   Tables,
   QueryFilters,
   QueryArgs,
   UpdateArgs,
   InsertArgs,
 } from "../../types";
-
-type TokenId = {
-  queryBy: "token_id";
-  id: number;
-};
-
-type SessionId = {
-  queryBy: "session_id";
-  sessionId: number;
-};
-
-type TokenHash = {
-  queryBy: "token_hash";
-  tokenHash: string;
-};
-
-type QueryOptions = {
-  dbOrTx?: DbContext | TxContext | undefined;
-} & (TokenId | SessionId | TokenHash);
 
 export class SessionTokenRepository extends Repository<Tables.SessionTokens> {
   public constructor(context: DbContext) {
@@ -41,63 +20,14 @@ export class SessionTokenRepository extends Repository<Tables.SessionTokens> {
     return await args.fn(insert, this._buildWhereClause);
   }
 
-  //  todo: remove this
-  public async insertOne({
-    dbOrTx,
-    sessionToken,
-  }: {
-    dbOrTx: DbContext | TxContext | undefined;
-    sessionToken: InsertModels.SessionToken;
-  }): Promise<number | undefined> {
-    const inserted = await this._insertOne({ dbOrTx, value: sessionToken });
-    return inserted?.id;
-  }
-
   public async execQuery<T>(args: QueryArgs.SessionToken<T>) {
     const query = (args.dbOrTx ?? this._dbContext).query.sessionTokens;
     return await args.fn(query, this._buildWhereClause);
   }
 
-  //  todo: remove this
-  public async getMany(
-    queryOptions: {
-      isAscending?: boolean;
-      pageSize?: number;
-      pageNumber?: number;
-    } & QueryOptions
-  ): Promise<ViewModels.SessionToken[]> {
-    const whereClause = this.buildWhereClause(queryOptions);
-
-    const tokens = await this._getMany({
-      column: sessionTokens.sessionId,
-      whereClause,
-      ...queryOptions,
-    });
-
-    return tokens;
-  }
-
   public async execUpdate<T>(args: UpdateArgs.SessionToken<T>) {
     const update = (args.dbOrTx ?? this._dbContext).update(sessionTokens);
     return await args.fn(update, this._buildWhereClause);
-  }
-
-  //  todo: remove this
-  public async invalidateTokens(
-    queryOptions: QueryOptions
-  ): Promise<ViewModels.SessionToken[]> {
-    const whereClause = this.buildWhereClause(queryOptions);
-    const { dbOrTx = this._dbContext } = queryOptions;
-
-    const updatedTokens = await dbOrTx
-      .update(sessionTokens)
-      .set({
-        isUsed: true,
-      })
-      .where(whereClause)
-      .returning();
-
-    return updatedTokens;
   }
 
   private _buildWhereClause(
@@ -121,19 +51,5 @@ export class SessionTokenRepository extends Repository<Tables.SessionTokens> {
     }
 
     return undefined;
-  }
-
-  //  todo: remove this
-  private buildWhereClause(queryOptions: QueryOptions) {
-    switch (queryOptions.queryBy) {
-      case "token_id":
-        return eq(sessionTokens.id, queryOptions.id);
-      case "session_id":
-        return eq(sessionTokens.sessionId, queryOptions.sessionId);
-      case "token_hash":
-        return eq(sessionTokens.tokenHash, queryOptions.tokenHash);
-      default:
-        throw new Error("Invalid query method.");
-    }
   }
 }
