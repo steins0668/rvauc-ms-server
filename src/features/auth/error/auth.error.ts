6933a724c3253a4bc73113a4a7631917b0b10c2c
steins0668 <archelausagaton@gmail.com>
2025-11-01 18:22:49 +0800

    chore: remove unused error classes

--- src/features/auth/error/auth.error.ts
+++ src/features/auth/error/auth.error.ts
@@ -1,5 +1,3 @@
 export * from "./auth-config.error";
-export * from "./register.error";
 export * from "./session.error";
 export * from "./sign-in.error";
-export * from "./user-data.error";
--- src/features/auth/error/register.error.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError, type DbAccess } from "../../../error";
-
-export namespace Register {
-  export type ErrorName =
-    | "REGISTER_USER_ALREADY_EXISTS_ERROR"
-    | DbAccess.ErrorName;
-
-  export class ErrorClass extends BaseError<ErrorName> {}
-}
