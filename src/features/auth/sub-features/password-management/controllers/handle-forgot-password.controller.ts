231278e4ef48fbb83bc93b08463b54a9d6251cbf
steins0668 <archelausagaton@gmail.com>
2025-11-02 20:39:49 +0800

    feat: add `handleForgotPassword` controller for forgot password requests

---
+++ src/features/auth/sub-features/password-management/controllers/handle-forgot-password.controller.ts
@@ -0,0 +1,139 @@
+import { Request, Response } from "express";
+import crypto from "crypto";
+import { ResultBuilder } from "../../../../../utils";
+import { AuthError } from "../../../error";
+import { UserDataService } from "../../../services";
+import { Schemas } from "../schemas";
+import { Utils } from "../utils";
+import { AuthenticationResult } from "../../../types";
+
+export async function handleForgotPassword(
+  req: Request<{}, {}, Schemas.ForgotPassword>,
+  res: Response
+) {
+  const { body, userDataService, requestLogger: logger } = req;
+  //  * get user based on POST email
+  logger.log("debug", "Verifying user...");
+  const verification = await verifyUser({ userDataService, email: body.email });
+
+  if (!verification.success) {
+    const { error } = verification;
+
+    res
+      .status(AuthError.Authentication.getErrStatusCode(error))
+      .json({ success: false, message: error.message });
+
+    const safeId = body.email.replace(/^(.{2}).*(@.*)$/, "$1***$2"); //  mask emails
+    const logMsg = `Failed sign-in attempt from user ${safeId}.`;
+    logger.log("error", logMsg, error);
+
+    return;
+  }
+
+  const user = verification.result;
+
+  //  * generate random reset token
+  logger.log("debug", "Generating reset token...");
+  const token = crypto.randomBytes(32).toString("hex");
+  const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+  const tokenCreation = await req.passwordManagementService.storeResetToken(
+    user.id,
+    tokenHash
+  );
+
+  if (!tokenCreation.success) {
+    const { error } = tokenCreation;
+    const message = "Failed generating reset token. Please try again later.";
+
+    res
+      .status(AuthError.Authentication.getErrStatusCode(error))
+      .json({ success: false, message });
+
+    logger.log("error", error.message, error);
+
+    return;
+  }
+
+  //  * send reset url to email
+  logger.log("debug", "Sending reset url...");
+  const emailTransport = await sendEmail({
+    req,
+    resetToken: token,
+    email: user.email,
+  });
+
+  if (!emailTransport.success) {
+    //  * remove reset token from db
+    const { id } = tokenCreation.result;
+    await req.passwordManagementService.deleteResetToken(id);
+
+    const { error } = emailTransport;
+
+    const message = "Failed sending reset url. Please try again later.";
+    res
+      .status(AuthError.Authentication.getErrStatusCode(error))
+      .json({ success: false, message });
+
+    logger.log("error", error.message, error);
+
+    return;
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Reset password url sent. Please check your email.",
+  });
+}
+
+async function verifyUser(args: {
+  email: string;
+  userDataService: UserDataService;
+}) {
+  const query = await args.userDataService.queryUsers({
+    fn: async (query, converter) => {
+      const { email } = args;
+      return await query.findFirst({
+        where: converter({ email }),
+      });
+    },
+  });
+
+  if (query.success) return query;
+
+  return ResultBuilder.fail(
+    AuthError.Authentication.normalizeError({
+      name: "AUTHENTICATION_SIGN_IN_VERIFICATION_ERROR",
+      message: "Could not find user.",
+      err: query.error,
+    })
+  );
+}
+
+async function sendEmail(args: {
+  req: Request<{}, {}, Schemas.ForgotPassword>;
+  resetToken: string;
+  email: string;
+}): Promise<AuthenticationResult.Success<null> | AuthenticationResult.Fail> {
+  try {
+    const { req, resetToken, email } = args;
+    const { protocol } = req;
+    const host = req.get("host");
+    const resetUrl = `${protocol}://${host}/auth/reset-password/${resetToken}`;
+    const subject = "Password change request received.";
+    const text = `We have received a password reset request. Please use the link below to reset your password\n\n${resetUrl}\n\nThis link will be valid for 10 minutes only.`;
+    await Utils.EmailTransports.sendEmail({
+      to: email,
+      subject,
+      text,
+    });
+    return ResultBuilder.success(null);
+  } catch (err) {
+    return ResultBuilder.fail(
+      AuthError.Authentication.normalizeError({
+        name: "AUTHENTICATION_PASSWORD_RESET_EMAIL_ERROR",
+        message: "Failed sending reset url.",
+        err,
+      })
+    );
+  }
+}
