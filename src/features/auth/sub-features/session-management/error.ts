bec7ffe684c48bee089fdfbf882ab6a2e64e1817
steins0668 <archelausagaton@gmail.com>
2025-11-05 22:53:57 +0800

    wip: `validateJwt` middleware

--- src/features/auth/sub-features/session-management/error.ts
+++ src/features/auth/sub-features/session-management/error.ts
@@ -1,6 +1,6 @@
 import { BaseError } from "../../../../error";
 import { Exhaustive } from "../../../../types";
-import { StatusCode } from "../../../../utils";
+import { isError, StatusCode } from "../../../../utils";
 
 export namespace CustomError {
   export namespace Config {
@@ -14,6 +14,31 @@ export namespace CustomError {
       AUTH_CONFIG_COOKIE_CONFIG_ERROR: 500,
       AUTH_CONFIG_ENV_TKN_SECRET_ERROR: 500,
     } as const;
+
+    export function normalizeError<E extends ErrorName>({
+      name,
+      message,
+      err,
+    }: {
+      name: E;
+      message: string;
+      err: unknown;
+    }) {
+      if (isError(ErrorClass, err)) return err;
+
+      return new ErrorClass({
+        name,
+        message,
+        cause:
+          err instanceof Error
+            ? {
+                name: err.name,
+                message: err.message,
+                stack: err.stack,
+              }
+            : err,
+      });
+    }
 
     export function getErrStatusCode(error: ErrorClass) {
       return StatusCode.fromError({
