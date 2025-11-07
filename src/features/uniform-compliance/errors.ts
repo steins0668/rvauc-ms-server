79e88c9afcd3aaf106231e028326d1fc65a428ce
steins0668 <archelausagaton@gmail.com>
2025-11-07 15:48:27 +0800

    feat: add `ComplianceData` error class

---
+++ src/features/uniform-compliance/errors.ts
@@ -0,0 +1,46 @@
+import { BaseError } from "../../error";
+import { Exhaustive } from "../../types";
+import { isError, StatusCode } from "../../utils";
+
+export namespace Errors {
+  export namespace ComplianceData {
+    export type ErrorName = "COMPLIANCE_DATA_RECORD_QUERY_ERROR";
+
+    export class ErrorClass extends BaseError<ErrorName> {}
+
+    export const statusCodeMap: Exhaustive<ErrorName> = {
+      COMPLIANCE_DATA_RECORD_QUERY_ERROR: 500,
+    };
+
+    export function normalizeError<E extends ErrorName>({
+      name,
+      message,
+      err,
+    }: {
+      name: E;
+      message: string;
+      err: unknown;
+    }) {
+      if (isError(ErrorClass, err)) return err;
+
+      return new ErrorClass({
+        name,
+        message,
+        cause:
+          err instanceof Error
+            ? {
+                name: err.name,
+                message: err.message,
+                stack: err.stack,
+              }
+            : err,
+      });
+    }
+    export function getErrStatusCode(error: ErrorClass) {
+      return StatusCode.fromError({
+        errorName: error.name,
+        statusCodeMap,
+      });
+    }
+  }
+}
