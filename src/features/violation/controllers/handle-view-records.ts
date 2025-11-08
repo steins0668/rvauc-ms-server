1fc36df5d69849000315c8c2b7a71c6a6a239c5c
steins0668 <archelausagaton@gmail.com>
2025-11-08 22:41:25 +0800

    feature: implement db transaction and dto conversion in `handleViewRecords` controller

--- src/features/violation/controllers/handle-view-records.ts
+++ src/features/violation/controllers/handle-view-records.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
-import { Auth } from "../../auth";
-import { Services } from "../services";
+import { Enums } from "../../../data";
+import { execTransaction, TxContext } from "../../../db/create-context";
+import { BaseResult } from "../../../types";
 import { ResultBuilder } from "../../../utils";
+import { Auth } from "../../auth";
+import { Data } from "../data";
 import { Errors } from "../errors";
+import { Schemas } from "../schemas";
+import { Services } from "../services";
 import { Types } from "../types";
 
 export async function handleViewRecords(req: Request, res: Response) {
@@ -68,37 +73,37 @@ const recordsResolver = {
       { role: "student" }
     >;
   }) => {
-    const { userDataService, payload } = args;
+    const { userDataService, violationDataService, payload } = args;
 
-    //  todo: implement transaction to be able to apply offset and pagination for records
-    const query = await userDataService.queryStudents({
-      fn: async (query, converter) => {
-        const student = await query.findFirst({
-          where: converter({ studentNumber: payload.studentNumber }),
-          with: {
-            violationRecords: {
-              orderBy: (records, { desc }) => [desc(records.date)],
-              with: { status: true },
-              limit: 6,
-            },
-          },
-        });
+    const transaction = await execTransaction(async (tx) => {
+      const studentQuery = await fetchStudent({ tx, userDataService, payload });
 
-        return student;
-      },
+      if (!studentQuery.success) return studentQuery; //  ! propagate error
+
+      const studentId = studentQuery.result;
+      const recordsQuery = await fetchRecords({
+        tx,
+        violationDataService,
+        studentId,
+      });
+
+      return recordsQuery;
     });
 
-    if (!query.success)
+    if (!transaction.success)
       //  ! propagate error
       return ResultBuilder.fail(
         Errors.ViolationData.normalizeError({
           name: "VIOLATION_DATA_QUERY_RECORD_ERROR",
-          message: "Failed to get records",
-          err: query.error,
+          message: "Failed to get records.",
+          err: transaction.error,
         })
       );
 
-    return ResultBuilder.success(query.result.violationRecords);
+    const rawRecords = transaction.result;
+    const conversion = toDTORecord(rawRecords);
+
+    return conversion;
   },
 
   professor: async (args: {
@@ -110,3 +115,87 @@ const recordsResolver = {
     >;
   }) => [] as Types.Db.ViewModels.ViolationRecord[], //  todo: do this
 };
+
+async function fetchStudent(args: {
+  tx: TxContext;
+  userDataService: Auth.Core.Services.UserData.Service;
+  payload: Extract<
+    Auth.Core.Schemas.Payloads.AccessToken.RoleBased,
+    { role: "student" }
+  >;
+}) {
+  const { tx, userDataService, payload } = args;
+  return await userDataService.queryStudents({
+    dbOrTx: tx,
+    fn: async (query, converter) => {
+      return await query
+        .findFirst({
+          where: converter({ studentNumber: payload.studentNumber }),
+        })
+        .then((result) => result?.id);
+    },
+  });
+}
+
+async function fetchRecords(args: {
+  tx: TxContext;
+  violationDataService: Services.ViolationData.Service;
+  studentId: number;
+}) {
+  const { tx, violationDataService, studentId } = args;
+  return await violationDataService.queryRecord({
+    dbOrTx: tx,
+    fn: async (query, converter) => {
+      return await query.findMany({
+        where: converter({ studentId }),
+        orderBy: (records, { desc }) => [desc(records.date)],
+        with: { status: true },
+        limit: 6,
+        columns: {
+          studentId: false,
+          complianceRecordId: false,
+          number: false,
+          statusId: false,
+        },
+      });
+    },
+  });
+}
+
+type FetchRecordsResult = Awaited<ReturnType<typeof fetchRecords>>;
+type RawRecords = FetchRecordsResult extends
+  | BaseResult.Fail<infer _>
+  | BaseResult.Success<infer R>
+  ? R
+  : never;
+
+function toDTORecord(
+  rawRecords: RawRecords
+):
+  | Types.ViolationResult.Success<Schemas.ViolationData.RecordDTO[]>
+  | Types.ViolationResult.Fail {
+  try {
+    const dto = rawRecords.map((record) => {
+      const { id, date: isoDate } = record;
+      const rawDate = new Date(isoDate);
+      const date = isoDate.split("T")[0] as string;
+      const day = Enums.Days[rawDate.getDay()] as string;
+      const hours = rawDate.getHours().toString().padStart(2, "0");
+      const minutes = rawDate.getMinutes().toString().padStart(2, "0");
+      const time = hours + ":" + minutes; //  * hh:mm format
+      const status = record.status.name;
+      const reasons = record.reasons as Data.Enums.ViolationReason[];
+
+      return { id, date, day, time, status, reasons };
+    });
+    return ResultBuilder.success(dto);
+  } catch (err) {
+    return ResultBuilder.fail(
+      Errors.ViolationData.normalizeError({
+        name: "VIOLATION_DATA_QUERY_RECORD_ERROR",
+        message: "Failed converting records to DTO.",
+        err,
+      })
+    );
+  }
+}
