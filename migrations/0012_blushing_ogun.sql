5165e8ed825fe0d5fc0901da3729675fa7e57ca3
steins0668 <archelausagaton@gmail.com>
2025-11-08 22:05:23 +0800

    feat: add `notNull` constraint to `reasons` column of `violation_records

---
+++ migrations/0012_blushing_ogun.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_violation_records` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`student_id` integer NOT NULL,
+	`status_id` integer NOT NULL,
+	`number` text NOT NULL,
+	`date` text NOT NULL,
+	`reasons` text NOT NULL,
+	`compliance_record_id` integer,
+	FOREIGN KEY (`student_id`) REFERENCES `students`(`id`) ON UPDATE cascade ON DELETE restrict,
+	FOREIGN KEY (`status_id`) REFERENCES `violation_statuses`(`id`) ON UPDATE cascade ON DELETE restrict,
+	FOREIGN KEY (`compliance_record_id`) REFERENCES `compliance_records`(`id`) ON UPDATE cascade ON DELETE restrict
+);
+--> statement-breakpoint
+INSERT INTO `__new_violation_records`("id", "student_id", "status_id", "number", "date", "reasons", "compliance_record_id") SELECT "id", "student_id", "status_id", "number", "date", "reasons", "compliance_record_id" FROM `violation_records`;--> statement-breakpoint
+DROP TABLE `violation_records`;--> statement-breakpoint
+ALTER TABLE `__new_violation_records` RENAME TO `violation_records`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `violation_records_number_unique` ON `violation_records` (`number`);
