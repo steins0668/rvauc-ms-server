543a4bdc0ee4da05e8814a1d127430b7bb86b211
steins0668 <archelausagaton@gmail.com>
2025-11-10 15:24:16 +0800

    refactor: rename `rfid` field of `SignIn` schema to `rfidUid`

--- src/features/auth/sub-features/minimal-authentication/schemas.ts
+++ src/features/auth/sub-features/minimal-authentication/schemas.ts
@@ -7,7 +7,7 @@ export namespace Schemas {
     export const schema = z.discriminatedUnion("type", [
       //  * rfid based
       z.strictObject({
-        type: z.literal("rfid"),
+        type: z.literal("rfidUid"),
         rfidUid: z.string({
           error: (iss) => {
             if (iss.code === "invalid_type") return "RFID UID must be a string";
