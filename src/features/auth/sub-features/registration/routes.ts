c21f845cb03e57f3ac72cdbeaec2776400b776bd
steins0668 <archelausagaton@gmail.com>
2025-11-11 15:57:20 +0800

    feat(registration): add routes

---
+++ src/features/auth/sub-features/registration/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { validateRequest } from "../../../../middlewares";
+import { Controllers } from "./controllers";
+import { Schemas } from "./schemas";
+
+export const Routes = Router();
+
+Routes.post(
+  "/register",
+  validateRequest(Schemas.Register.roleBased),
+  Controllers.handleRegister
+);
