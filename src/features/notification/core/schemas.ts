c0b2b5ee8b7bf0a8779ebc2ba42978cd44d0441a
steins0668 <archelausagaton@gmail.com>
2025-11-19 14:56:21 +0800

    feat: add todo for `pushNotification` schema

--- src/features/notification/core/schemas.ts
+++ src/features/notification/core/schemas.ts
@@ -3,9 +3,9 @@ import z from "zod";
 export namespace Schemas {
   export const pushNotification = z.strictObject({
     userId: z.number(),
-    category: z.string(),
-    title: z.string(),
-    message: z.string(),
+    category: z.string(), //  todo: add better validation
+    title: z.string(), //  todo: add better validation
+    message: z.string(), //  todo: add better validation
   });
 
   export type PushNotification = z.infer<typeof pushNotification>;
