97689b9961c64770188809f00ca4669c5582289b
steins0668 <archelausagaton@gmail.com>
2025-11-20 18:53:57 +0800

    feat: add `clearNotifications` function in `Notification` service

--- src/features/notification/core/services.ts
+++ src/features/notification/core/services.ts
@@ -6,6 +6,36 @@ import { Types } from "./types";
 
 export namespace Services {
   export namespace Notification {
+    export async function clearNotifications(args: { userId: number }) {
+      type ResponseType = Types.NotificationMicroservice.Response.Union<null>;
+
+      try {
+        const result = await Utils.notifClient.post(
+          "/notifications/clear-notifications",
+          args
+        );
+
+        const { success, message } = result.data;
+
+        return success
+          ? ResultBuilder.success(message)
+          : ResultBuilder.fail(
+              new Errors.Notification.ErrorClass({
+                name: "NOTIFICATION_FAILED_CLEARING_NOTIFICATIONS_ERROR",
+                message: message ?? "Failed clearing notifications.",
+              })
+            );
+      } catch (err) {
+        return ResultBuilder.fail(
+          Errors.Notification.normalizeError({
+            name: "NOTIFICATION_FAILED_CLEARING_NOTIFICATIONS_ERROR",
+            message: "Failed clearing notifications",
+            err,
+          })
+        );
+      }
+    }
+
     export async function getNotifications(args: { userId: number }) {
       type ResponseType =
         Types.NotificationMicroservice.Response.Union<Schemas.PushNotification>;
