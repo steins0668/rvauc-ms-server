f998af181f87a168df15214ccecc5851997a5b2b
steins0668 <archelausagaton@gmail.com>
2025-11-20 22:40:18 +0800

    feat: add notification handling in `handleNewRecord`

--- src/features/uniform-compliance/controllers/handle-new-record.ts
+++ src/features/uniform-compliance/controllers/handle-new-record.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { execTransaction } from "../../../db/create-context";
 import { ResultBuilder } from "../../../utils";
 import { Auth } from "../../auth";
+import { Notification } from "../../notification";
 import { Violation } from "../../violation";
 import { Errors } from "../errors";
 import { Schemas } from "../schemas";
@@ -70,79 +71,123 @@ async function storeRecord(args: {
   | Types.ComplianceResult.Success<Types.Db.ViewModels.ComplianceRecord[]>
   | Types.ComplianceResult.Fail
 > {
-  const insertion = await execTransaction(async (tx) => {
-    const studentQuery = await args.userDataService.findStudentWhere({
-      dbOrTx: tx,
-      filter: { studentNumber: args.body.studentNumber },
-    });
+  let notifications = [] as ReturnType<typeof notify>[];
 
-    if (!studentQuery.success) return studentQuery; //  ! propagate error
-
-    const now = new Date().toISOString();
-    const storeComplianceRecord = await args.complianceDataService.storeRecords(
-      {
+  try {
+    const result = await execTransaction(async (tx) => {
+      const studentQuery = await args.userDataService.findStudentWhere({
         dbOrTx: tx,
-        values: [
-          {
-            studentId: studentQuery.result.id,
-            ...args.body,
-            createdAt: now,
-          },
-        ],
-      }
-    );
+        filter: { studentNumber: args.body.studentNumber },
+      });
 
-    if (!storeComplianceRecord.success) return storeComplianceRecord; // ! propagate error
+      if (!studentQuery.success) throw studentQuery.error; //  ! propagate error
 
-    const { studentNumber, termId, uniformTypeId, ...flags } = args.body;
-    const hasViolation = Object.values(flags).some((value) => !value);
-
-    if (hasViolation) {
-      const { id: complianceRecordId } = storeComplianceRecord
-        .result[0] as Types.Db.ViewModels.ComplianceRecord;
-      const { id: studentId } = studentQuery.result;
-
-      const reasons = [];
-
-      if (!flags.hasId)
-        reasons.push(Violation.Data.Records.ViolationReason.noId);
-      if (!flags.validBottoms)
-        reasons.push(Violation.Data.Records.ViolationReason.incorrectBottoms);
-      if (!flags.validFootwear)
-        reasons.push(Violation.Data.Records.ViolationReason.incorrectFootwear);
-      if (!flags.validUpperwear)
-        reasons.push(Violation.Data.Records.ViolationReason.incorrectUpperwear);
-
-      const storeViolationRecord = await args.violationDataService.storeRecords(
-        {
+      const now = new Date();
+      const nowISO = now.toISOString();
+      const nowDateStr = now.toDateString(); //  * for logging and notif
+      const storeComplianceRecord =
+        await args.complianceDataService.storeRecords({
           dbOrTx: tx,
           values: [
             {
-              complianceRecordId,
-              date: now,
-              statusId: Violation.Data.Records.ViolationStatus.unsettled,
-              studentId,
-              reasons,
+              studentId: studentQuery.result.id,
+              ...args.body,
+              createdAt: nowISO,
             },
           ],
-        }
+        });
+
+      if (!storeComplianceRecord.success) throw storeComplianceRecord.error; // ! propagate error
+      const { id: studentId } = studentQuery.result;
+      const { id: complianceRecordId } = storeComplianceRecord
+        .result[0] as Types.Db.ViewModels.ComplianceRecord;
+
+      notifications.push(
+        notify({
+          category: "uniform_compliance_recorded",
+          userId: studentId,
+          title: "Uniform Compliance Record",
+          message: `Successfully recorded uniform compliance for ${nowDateStr}`,
+        })
       );
 
-      if (!storeViolationRecord.success) return storeViolationRecord; //  ! propagate error
-    }
+      const { studentNumber, termId, uniformTypeId, ...flags } = args.body;
+      const hasViolation = Object.values(flags).some((value) => !value);
 
-    return storeComplianceRecord;
-  });
+      if (hasViolation) {
+        const reasons = getViolationReasons(args.body);
 
-  if (!insertion.success)
-    //  ! propagate error
+        const storeViolationRecord =
+          await args.violationDataService.storeRecords({
+            dbOrTx: tx,
+            values: [
+              {
+                complianceRecordId,
+                date: nowISO,
+                statusId: Violation.Data.Records.ViolationStatus.unsettled,
+                studentId,
+                reasons,
+              },
+            ],
+          });
+
+        if (!storeViolationRecord.success) throw storeViolationRecord.error; //  ! propagate error
+
+        const reasonsStr = reasons.toString();
+        notifications.push(
+          notify({
+            category: "violation_recorded",
+            userId: studentId,
+            title: "Violation Record",
+            message: `A uniform compliance violation has been detected for the following reasons: ${reasonsStr} `,
+          })
+        );
+      }
+
+      return storeComplianceRecord;
+    });
+
+    // for (const notification of notifications) await notification;  //  todo: add message queues so you can implement this
+    await Promise.all(notifications);
+
+    return result;
+  } catch (err) {
     return ResultBuilder.fail(
       Errors.ComplianceData.normalizeError({
         name: "COMPLIANCE_DATA_STORE_RECORD_ERROR",
         message: "Failed storing record",
-        err: insertion.error,
+        err,
       })
     );
+  }
+}
 
-  return insertion;
+function getViolationReasons(complianceData: Schemas.ComplianceData.NewRecord) {
+  const { studentNumber, termId, uniformTypeId, ...flags } = complianceData;
+  const reasons = [];
+
+  if (!flags.hasId) reasons.push(Violation.Data.Records.ViolationReason.noId);
+  if (!flags.validBottoms)
+    reasons.push(Violation.Data.Records.ViolationReason.incorrectBottoms);
+  if (!flags.validFootwear)
+    reasons.push(Violation.Data.Records.ViolationReason.incorrectFootwear);
+  if (!flags.validUpperwear)
+    reasons.push(Violation.Data.Records.ViolationReason.incorrectUpperwear);
+
+  return reasons;
+}
+
+async function notifyInternalError(args: { userId: number; message?: string }) {
+  return await notify({
+    category: "internal_error",
+    userId: args.userId,
+    title: "Internal error.",
+    message: args.message ?? "Something went wrong. Please try again later.",
+  });
+}
+
+async function notify(
+  notification: Notification.Core.Schemas.PushNotification
+) {
+  return await Notification.Core.Services.Api.pushNotification(notification);
 }
