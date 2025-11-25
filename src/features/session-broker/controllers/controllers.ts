6004eab461ea5038dd1ac560c7d75a5c2f0ad273
steins0668 <archelausagaton@gmail.com>
2025-11-25 16:12:04 +0800

    feat: add controllers

---
+++ src/features/session-broker/controllers/controllers.ts
@@ -0,0 +1,2 @@
+export * from "./handle-get-active-token.controller";
+export * from "./handle-sign-in.controller";
---
+++ src/features/session-broker/controllers/handle-get-active-token.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { Errors } from "../errors";
+import { Schemas } from "../schemas";
+import { Services } from "../services";
+
+export async function handleGetActiveToken(
+  req: Request<Schemas.RequestParams.StationName>,
+  res: Response
+) {
+  const { stationAuth, params, requestLogger: logger } = req;
+
+  logger.log(
+    "info",
+    `Attempting to retrieve token for station: ${stationAuth.stationName}...`
+  );
+
+  const retrieved = await Services.SessionBroker.getToken(params.stationName);
+
+  if (!retrieved.success) {
+    const { error } = retrieved;
+    logger.log("error", "Failed retrieving token.", error);
+
+    return res
+      .status(Errors.Session.getErrStatusCode(error))
+      .json({ success: false, message: "Failed retrieving token." });
+  }
+
+  logger.log("info", "Token retrieved.");
+
+  res.status(200).json({ success: true, result: retrieved.result });
+}
