c58fcff50eeb4fe804d8c29969fbf1a361ea470f
steins0668 <archelausagaton@gmail.com>
2025-12-03 03:18:57 +0800

    feat: enhance data handling in `handleGetActiveToken` controller

--- src/features/session-broker/controllers/handle-get-active-token.controller.ts
+++ src/features/session-broker/controllers/handle-get-active-token.controller.ts
@@ -25,6 +25,17 @@ export async function handleGetActiveToken(
       .json({ success: false, message: "Failed retrieving token." });
   }
 
+  if (retrieved.success && !retrieved.result) {
+    logger.log(
+      "info",
+      `Failed retrieving token. ${stationAuth.stationName} has no active tokens.`
+    );
+
+    return res
+      .status(404)
+      .json({ success: false, message: "Failed retrieving token." });
+  }
+
   logger.log("info", "Token retrieved.");
 
   res.status(200).json({ success: true, result: retrieved.result });
