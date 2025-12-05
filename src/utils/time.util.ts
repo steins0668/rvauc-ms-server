d9c1d8b879fe3b0ed9dc40a2bdf688a08c4ec0e8
steins0668 <archelausagaton@gmail.com>
2025-12-06 02:31:55 +0800

    refactor: use en-PH locale for converting to time strings

--- src/utils/time.util.ts
+++ src/utils/time.util.ts
@@ -33,7 +33,7 @@ export namespace TimeUtil {
    * @returns
    */
   export const toPhTime = (date: Date) =>
-    new Intl.DateTimeFormat("sv-SE", {
+    new Intl.DateTimeFormat("en-PH", {
       timeZone: "Asia/Manila",
       hour: "2-digit",
       minute: "2-digit",
