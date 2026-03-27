51b16a831a48045462b6c0d18a1e9295b3328790
steins0668 <archelausagaton@gmail.com>
2026-03-27 19:52:26 +0800

    feat: add `not null` constraint to `rooms` table `name` column

--- src/models/rooms.ts
+++ src/models/rooms.ts
@@ -9,7 +9,7 @@ export const rooms = sqliteTable(
   "rooms",
   {
     id: integer("id").primaryKey({ autoIncrement: true }),
-    name: text("name"),
+    name: text("name").notNull(),
     building: text("building"),
   },
   (t) => [uniqueIndex("uidx_rooms_name_building").on(t.name, t.building)],
