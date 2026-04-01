e28127acf2f3c3fe7a5b118cf91ae23d8d987365
steins0668 <archelausagaton@gmail.com>
2026-04-01 23:24:28 +0800

    feat: implement handler for retrieving current or next class schedule (#48)

---
+++ src/features/enrollments/subfeatures/schedule/controllers/handle-get-current-or-next.controller.ts
@@ -0,0 +1,88 @@
+import { Request, Response } from "express";
+import { StrictValidatedRequest } from "../../../../../interfaces";
+import { Auth } from "../../../../auth";
+import { Core } from "../../../core";
+import { Schemas } from "../schemas";
+
+const internalErrMessage = "Something went wrong. Please try again later.";
+
+export async function handleGetCurrentOrNext(req: Request, res: Response) {
+  const {
+    auth,
+    validated: { query },
+    classSchedService,
+    termDataService,
+    requestLogger: logger,
+  } = req as StrictValidatedRequest<
+    {},
+    {},
+    {},
+    Schemas.RequestQuery.UserSchedule
+  >;
+
+  logger.log("info", "Attempting to get schedule...");
+  //  * authorize user
+  const isAllowedPayload = Auth.Core.Utils.ensureAllowedPayload(auth, "full");
+
+  if (!isAllowedPayload) {
+    logger.log(
+      "info",
+      "Invalid payload attempted to access `enrollments/schedule/get-schedule`.",
+    );
+
+    return res.status(403).json({
+      success: false,
+      message: "You are not allowed to access this resource.",
+    });
+  }
+
+  let term;
+
+  logger.log("debug", "Attempting to get current term from system config...");
+  try {
+    const queried = await termDataService.getCurrentTerm();
+    if (!queried.success) throw queried.error;
+
+    term = queried.result;
+    logger.log("debug", "Success getting term config: " + JSON.stringify(term));
+  } catch (err) {
+    logger.log("error", "Failed getting current term from system config.", err);
+
+    return res.status(500).json({
+      success: false,
+      message: internalErrMessage,
+    });
+  }
+
+  const date = new Date(query.date);
+  const { payload: user } = auth;
+
+  const queried = await classSchedService.getForNowOrNext({
+    userId: user.id,
+    role: user.role,
+    date,
+    termId: term.id,
+  });
+
+  if (!queried.success) {
+    const { error } = queried;
+
+    logger.log("error", "Failed querying enrollments.", error);
+
+    const message =
+      error.name === "ENROLLMENT_DATA_NO_CLASS_TODAY_ERROR"
+        ? `This ${user.role} does not have any more classes scheduled for this day.`
+        : internalErrMessage;
+
+    return res.status(Core.Errors.EnrollmentData.getErrStatusCode(error)).json({
+      success: false,
+      message,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    result: { schedule: queried.result },
+    message: "Success retrieving class.",
+  });
+}
