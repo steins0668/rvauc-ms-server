a21a1a197377805823dbd86473f5ac7a483ac619
steins0668 <archelausagaton@gmail.com>
2026-04-03 21:06:59 +0800

    refactor: rename file

--- src/features/enrollments/subfeatures/attendance/controllers/controllers.ts
+++ src/features/enrollments/subfeatures/attendance/controllers/controllers.ts
@@ -1,3 +1,3 @@
-export * from "./handle-new-record.controller";
+export * from "./handle-new-rfid-scan.controller";
 export * from "./handle-submit-records.controller";
 export * from "./handle-view-records.controller";
