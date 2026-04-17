0f0d284a525d04a2623b274e779f109266750322
steins0668 <archelausagaton@gmail.com>
2026-04-18 00:47:27 +0800

    feat: add ClassSessionDataService middleware to routes

--- src/features/enrollments/routes.ts
+++ src/features/enrollments/routes.ts
@@ -6,6 +6,7 @@ import { Schedule } from "./subfeatures/schedule";
 export const Routes = Router();
 
 Routes.use(Core.Middlewares.attachClassScheduleService);
+Routes.use(Core.Middlewares.attachClassSessionDataService);
 Routes.use(Core.Middlewares.attachClassSessionRuntimeService);
 Routes.use(Core.Middlewares.attachTermDataService);
 
