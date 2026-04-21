014ac94111118809032f073dc9fa37fb0a63f121
steins0668 <archelausagaton@gmail.com>
2026-04-21 08:27:03 +0800

    refactor: reorganize error types and add error translation function

--- src/features/enrollments/core/errors.ts
+++ src/features/enrollments/core/errors.ts
@@ -4,25 +4,39 @@ import { isError, StatusCode } from "../../../utils";
 
 export namespace Errors {
   export namespace EnrollmentData {
-    export type ErrorName =
+    export type Database =
+      | "ENROLLMENT_DATA_QUERY_ERROR"
+      | "ENROLLMENT_DATA_STORE_ERROR"
+      | "ENROLLMENT_DATA_UPDATE_ERROR"
+      | "ENROLLMENT_DATA_TRANSACTION_ERROR";
+
+    export type EntityNotFound =
       | "ENROLLMENT_DATA_CLASS_NOT_FOUND_ERROR"
       | "ENROLLMENT_DATA_CLASS_OFFERING_NOT_FOUND_ERROR"
       | "ENROLLMENT_DATA_CLASS_SESSION_NOT_FOUND_ERROR"
-      | "ENROLLMENT_DATA_DTO_CONVERSION_ERROR"
       | "ENROLLMENT_DATA_ENROLLMENT_NOT_FOUND_ERROR"
-      | "ENROLLMENT_DATA_INCONSISTENT_STATE_ERROR"
+      | "ENROLLMENT_DATA_STUDENT_NOT_FOUND_ERROR"
+      | "ENROLLMENT_DATA_TERM_NOT_FOUND_ERROR";
+
+    export type DomainState =
+      | "ENROLLMENT_DATA_STUDENT_NOT_ENROLLED_ERROR"
       | "ENROLLMENT_DATA_NO_ACTIVE_CLASS_ERROR"
       | "ENROLLMENT_DATA_NO_CLASS_LIST_ERROR"
       | "ENROLLMENT_DATA_NO_CLASS_TODAY_ERROR"
-      | "ENROLLMENT_DATA_QUERY_ERROR"
-      | "ENROLLMENT_DATA_STORE_ERROR"
-      | "ENROLLMENT_DATA_STUDENT_NOT_ENROLLED_ERROR"
-      | "ENROLLMENT_DATA_STUDENT_NOT_FOUND_ERROR"
+      | "ENROLLMENT_DATA_INCONSISTENT_STATE_ERROR";
+
+    export type SystemFailure =
       | "ENROLLMENT_DATA_SYSTEM_ERROR"
-      | "ENROLLMENT_DATA_TERM_NOT_FOUND_ERROR"
-      | "ENROLLMENT_DATA_TERM_RESOLUTION_ERROR"
-      | "ENROLLMENT_DATA_TRANSACTION_ERROR"
-      | "ENROLLMENT_DATA_UPDATE_ERROR";
+      | "ENROLLMENT_DATA_TERM_RESOLUTION_ERROR";
+
+    export type ApplicationFailure = "ENROLLMENT_DATA_DTO_CONVERSION_ERROR";
+
+    export type ErrorName =
+      | Database
+      | EntityNotFound
+      | DomainState
+      | SystemFailure
+      | ApplicationFailure;
 
     export class ErrorClass extends BaseError<ErrorName> {}
 
@@ -71,6 +85,23 @@ export namespace Errors {
             : err,
       });
     }
+
+    export function translateError<E extends ErrorName>(args: {
+      error: {
+        name: E;
+        message: string;
+        err: unknown;
+      };
+      match: (err: ErrorClass) => boolean;
+      map: (err: ErrorClass) => ErrorClass;
+    }) {
+      const normalized = normalizeError(args.error);
+
+      if (!args.match(normalized)) return normalized;
+
+      return args.map(normalized);
+    }
+
     export function getErrStatusCode(error: ErrorClass) {
       return StatusCode.fromError({
         errorName: error.name,
