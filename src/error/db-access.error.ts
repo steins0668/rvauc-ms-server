de6693df8c8cc99538864f2566751afbd1edeb55
steins0668 <archelausagaton@gmail.com>
2026-04-21 16:28:28 +0800

    fix: remove unused DB_ACCESS_NOT_FOUND_ERROR from error names

--- src/error/db-access.error.ts
+++ src/error/db-access.error.ts
@@ -7,7 +7,6 @@ export namespace DbAccess {
     | "DB_ACCESS_EXCEEDED_MAX_FETCH_RETRIES_ERROR"
     | "DB_ACCESS_EMPTY_TABLE_ERROR"
     | "DB_ACCESS_INSERT_ERROR"
-    | "DB_ACCESS_NOT_FOUND_ERROR"
     | "DB_ACCESS_QUERY_ERROR"
     | "DB_ACCESS_UPDATE_ERROR";
   export class ErrorClass extends BaseError<ErrorName> {}
