807259ffd9efccc00b82dd81f9a5a8e410c7d5c8
steins0668 <archelausagaton@gmail.com>
2026-04-23 22:14:47 +0800

    refactor: add JSDoc comments for route handlers to improve documentation

--- src/features/enrollments/subfeatures/schedule/routes.ts
+++ src/features/enrollments/subfeatures/schedule/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { Controllers } from "./controllers";
 import { validateRequest } from "../../../../middlewares";
+import { Core } from "../../core";
 import { Schemas } from "./schemas";
 import { Auth } from "../../../auth";
 
 export const Routes = Router();
 
+/**
+ * GET
+ *
+ * @returns {Core.Schemas.Dto.ScheduledClassesWithProfessor}
+ */
 Routes.get(
   "/get-schedule",
   Auth.Core.Middlewares.validateJwt("full"),
@@ -13,6 +19,11 @@ Routes.get(
   Controllers.handleGetSchedule,
 );
 
+/**
+ * GET
+ *
+ * @returns {Core.Schemas.Dto.ScheduledSessionWithProfessor}
+ */
 Routes.get(
   "/get-schedule/current-or-next",
   Auth.Core.Middlewares.validateJwt("full"),
@@ -20,6 +31,11 @@ Routes.get(
   Controllers.handleGetCurrentOrNext,
 );
 
+/**
+ * GET
+ *
+ * @returns {Core.Schemas.Dto.ScheduledClassesWithProfessor}
+ */
 Routes.get(
   "/get-class-list",
   Auth.Core.Middlewares.validateJwt("full"),
