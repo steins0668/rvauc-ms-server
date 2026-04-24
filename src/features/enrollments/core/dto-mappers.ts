7d40aea234714d46fe9f2d54d5fdbdc74ec1669d
steins0668 <archelausagaton@gmail.com>
2026-04-24 13:54:20 +0800

    refactor: remove unused ClassSchedule mapping for cleaner code structure

--- src/features/enrollments/core/dto-mappers.ts
+++ src/features/enrollments/core/dto-mappers.ts
@@ -6,44 +6,6 @@ import { Services } from "./services";
 
 export namespace DtoMappers {
   export namespace Query {
-    export namespace ClassSchedule {
-      export function map(
-        co: NonNullable<
-          Awaited<
-            ReturnType<Repositories.ClassOffering["queryWithClassAndProfessor"]>
-          >[0]
-        >,
-      ): Schemas.Dto.ScheduledClassWithProfessor {
-        const { class: cls, rooms: r } = co;
-        const { course: c, professor: p } = co.class;
-
-        const room = r ? r.building + r.name : "N/A";
-
-        return {
-          id: cls.id,
-          classNumber: cls.classNumber,
-          course: c,
-          offering: {
-            id: co.id,
-            weekDay: co.weekDay,
-            room,
-            startTimeText: co.startTimeText,
-            endTimeText: co.endTimeText,
-            startTime: co.startTime,
-            endTime: co.endTime,
-          },
-          professor: {
-            surname: p.user.surname,
-            firstName: p.user.firstName,
-            middleName: p.user.middleName,
-            gender: p.user.gender,
-            college: p.college.name,
-            facultyRank: p.facultyRank,
-          },
-        };
-      }
-    }
-
     export namespace ClassList {
       export function mapStudentView(
         rawList: Awaited<
