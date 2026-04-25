e50e9dae69cc2975b1937d24083f2625cd0f8342
steins0668 <archelausagaton@gmail.com>
2026-04-25 16:05:50 +0800

    refactor: remove unused professor, student, class, and course schemas for clarity

--- src/features/enrollments/core/schemas.ts
+++ src/features/enrollments/core/schemas.ts
@@ -1,55 +1,7 @@
 import z from "zod";
-import { Auth } from "../../auth";
 
 export namespace Schemas {
   export namespace Dto {
-    export const professor = z
-      .strictObject({
-        //  * professor data
-        college: z.string(),
-        facultyRank: z.string(),
-        //  * user data
-        surname: z.string(),
-        firstName: z.string(),
-        middleName: z.string(),
-        gender: z.string(),
-      })
-      .strip();
-
-    export const student = z
-      .strictObject({
-        //  * student data
-        studentNumber: z.string(),
-        department: z.string(),
-        yearLevel: z.number(),
-        block: z.string(),
-        //  * user data
-        surname: z.string(),
-        firstName: z.string(),
-        middleName: z.string(),
-        gender: z.string(),
-      })
-      .strip();
-
-    export const class_ = z
-      .strictObject({
-        id: z.number(),
-        classNumber: z.coerce.string(), //  ! coerced incase column type changes
-      })
-      .strip();
-
-    export const classOffering = z
-      .strictObject({
-        id: z.number(),
-        weekDay: z.string(),
-        room: z.preprocess((r) => r ?? undefined, z.string().default("N/A")),
-        startTimeText: z.string(),
-        endTimeText: z.string(),
-        startTime: z.number(),
-        endTime: z.number(),
-      })
-      .strip();
-
     export const classSession = z
       .strictObject({
         id: z.number(),
@@ -58,13 +10,6 @@ export namespace Schemas {
         datePh: z.string(),
         startTimeMs: z.number(),
         endTimeMs: z.number(),
-      })
-      .strip();
-
-    export const course = z
-      .strictObject({
-        code: z.coerce.string(), //  ! coerced incase column type changes
-        name: z.string(),
       })
       .strip();
 
